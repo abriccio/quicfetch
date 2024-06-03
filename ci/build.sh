ff97d24d3a40263f274c17dad1e1de81d9e7cb24
abriccio <alexbriccio@gmail.com>
2024-06-03 12:43:03 -0400

    RUNNER_OS is inviable as a variable here

--- ci/build.sh
+++ ci/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+[ $RUNNER_OS == 'macOS' ] && OS="macos"
+[ $RUNNER_OS == 'Windows' ] && OS="windows"
+[ $RUNNER_OS == 'Linux' ] && OS="linux"
+
 ZIG_VERSION="0.12.0"
 
 [ $(uname -m) == 'arm64' ] && ARCH='aarch64' || ARCH=$(uname -m)
@@ -10,8 +14,8 @@ TRIPLE="${RUNNER_OS}-${ARCH}-${ZIG_VERSION}"
 
 echo "Getting Zig ${TRIPLE}"
 
-[ $RUNNER_OS != 'windows' ] && curl "https://ziglang.org/builds/zig-${TRIPLE}.tar.xz" | tar xJ
-if [[ $RUNNER_OS == 'windows' ]]; then
+[ $RUNNER_OS != 'Windows' ] && curl "https://ziglang.org/builds/zig-${TRIPLE}.tar.xz" | tar xJ
+if [[ $RUNNER_OS == 'Windows' ]]; then
   choco install zig --version $ZIG_VERSION
   ZIG="zig.exe"
 else
