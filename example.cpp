58eaad2d84ad309e827ec60b795478fee442b095
abriccio <alexbriccio@gmail.com>
2024-06-15 18:00:56 -0400

    Fix weird global state stuff It's just a test

--- example.cpp
+++ example.cpp
@@ -14,40 +14,33 @@
 
 bool evil = true;
 bool g_need_update = false;
-bool can_quit = false;
 
 void on_check(Updater *u, bool needs_update) {
     if (needs_update) {
         printf("Needs update\n");
-        printf("%s\n", updater_get_message(u));
         g_need_update = true;
     }
     else
         printf("Does not need update\n");
-
-    evil = false;
 }
 
 void download_progress(Updater *u, size_t cur, size_t size) {
     printf("Downloaded : %zu / %zu\n", cur, size);
-    printf("%s\n", updater_get_message(u));
 }
 
 void download_finished(Updater *u, bool ok, size_t size) {
     if (ok) {
         printf("Download finished | %zuB\n", size);
-        printf("%s\n", updater_get_message(u));
     } else {
         printf("Download failed\n");
-        printf("%s\n", updater_get_message(u));
     }
-    can_quit = true;
+    evil = false;
 }
 
 int main() {
     Updater *updater = updater_init(
         "https://arborealaudio.com/versions/draft/index.json",
-        "OmniAmp",
+        "PiMax",
         "1.0.0"
     );
 
@@ -55,18 +48,16 @@ int main() {
 
     while (evil) {
         SLEEP(500);
+        if (g_need_update) {
+            updater_download_bin(updater, (DownloadOptions){
+                                     .progress = download_progress,
+                                     .finished = download_finished,
+                                     .dest_dir = NULL,
+                                     .chunk_size = 32 * 1024,
+                                 });
+        }
     }
 
-    if (g_need_update) {
-        updater_download_bin(updater, (DownloadOptions){
-                                 .progress = download_progress,
-                                 .finished = download_finished,
-                                 .dest_dir = NULL,
-                                 .chunk_size = 32 * 1024,
-                             });
-    }
-
-    while (!can_quit) {}
     updater_deinit(updater);
     return 0;
 }
