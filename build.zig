506fbecccf22f22c8c43a0987908e16c2422e784
abriccio <alexbriccio@gmail.com>
2024-12-20 15:57:08 -0500

    CI: I think we were missing quote literals

--- build.zig
+++ build.zig
@@ -34,7 +34,9 @@ pub fn build(b: *std.Build) !void {
         const api_key = std.posix.getenv("AWS_API_KEY") orelse return error.NoAPIKEY;
         const api_flag = try std.mem.concat(b.allocator, u8, &.{
             "-DAWS_API_KEY=",
+            "\"",
             api_key,
+            "\"",
         });
         const exe = b.addExecutable(.{
             .name = "main",
